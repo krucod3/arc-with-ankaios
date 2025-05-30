b7452d4badf5b31aefd0c744fe60359b2e13ff23
krucod3 <36224699+krucod3@users.noreply.github.com>
2025-05-30 10:25:10 +0000

    Fix bash script

--- clean-up-ankaios.sh
+++ clean-up-ankaios.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -e
 
 # Kill all ank-server and ank-agent processes
 pkill -f ank-server
 pkill -f ank-agent
 
 # Delete all podman containers
-podman rm -f $(podman ps -aq) 2>/dev/null
+podman rm -af 
 
 echo "Ankaios is stopped and all podman containers are deleted."
