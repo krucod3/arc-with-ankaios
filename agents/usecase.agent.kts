e904066da9186efb52e6cfe99e38ad310ec056da
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-08-25 15:25:21 +0200

    chore: add license to usecase-agent.

--- agents/usecase.agent.kts
+++ agents/usecase.agent.kts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import org.eclipse.lmos.arc.assistants.support.filters.UnresolvedDetector
 import org.eclipse.lmos.arc.assistants.support.filters.UseCaseResponseHandler
 
